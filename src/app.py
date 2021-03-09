03e409dd3aaee9683006ea2cd187ff23c24dd283
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-08 21:06:39 -0300

    carregando base de dados

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-import load_data
+from load_data import baixa_base_de_dados_casos_gerais
 import datetime
 
 hoje = datetime.date.today()
@@ -8,6 +8,6 @@ hoje = datetime.date.today()
 st.title('Covid no Estado do Paraná e região')
 
 @st.cache
-load_data.baixa_base_de_dados_casos_gerais(hoje.ano, hoje.month, hoje.day - 1)
+baixa_base_de_dados_casos_gerais(hoje.ano, hoje.month, hoje.day - 1)
 
 
