18c1a40586936af1812acf648eba9bc5db5e5d3a
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-08 22:03:25 -0300

    backup

--- src/app.py
+++ src/app.py
@@ -6,10 +6,6 @@ def main():
     hoje = datetime.date.today()
     st.title('Covid no Estado do Paraná e região')
     st.text(f'{hoje.day}/{hoje.month}/{hoje.year}',)
-    informa_covid = carrega_dados(hoje)
-
-    st.table(informa_covid)
-
 
 
 if __name__ == '__main__':
