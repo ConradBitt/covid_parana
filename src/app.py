5c87dddc0556cc5280f8be36299b11e233716821
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-09 10:52:23 -0300

    Adicionando figura com plotly

--- src/app.py
+++ src/app.py
@@ -30,17 +30,29 @@ def carrega_medias_moveis_cidades():
     medias_moveis = pd.read_csv(uri_medias_moveis, sep=';', engine='python')
     if 'DATA_CONFIRMACAO_DIVULGACAO' in medias_moveis.columns:
         medias_moveis['DATA_CONFIRMACAO_DIVULGACAO'] = pd.to_datetime(medias_moveis['DATA_CONFIRMACAO_DIVULGACAO'])
-        medias_moveis = medias_moveis.set_index('DATA_CONFIRMACAO_DIVULGACAO')
+        #medias_moveis = medias_moveis.set_index('DATA_CONFIRMACAO_DIVULGACAO')
 
     return medias_moveis
 
 def cidades_do_parana(dataframe):
     return dataframe.MUN_ATENDIMENTO.unique()
 
-def exibe_serie_temporal(dataframe, cidade, titulo):
-    fig = px.line(dataframe, x='DATA_CONFIRMACAO_DIVULGACAO', y='CASO_CONFIRMADO_NO_DIA')
+def exibe_evolucao_casos(dataframe, cidade):
+    casos_na_cidade = dataframe.query(f'MUN_ATENDIMENTO == "{cidade.upper()}"')
+    fig = px.line(
+        casos_na_cidade,
+        x = 'DATA_CONFIRMACAO_DIVULGACAO',
+        y = 'CASO_CONFIRMADO_NO_DIA',
+        color_discrete_sequence = px.colors.sequential.Cividis
+        )
     fig.layout.title.text = f'Evolução dos casos em {cidade.title()}'
-    
+    fig.layout.xaxis.title.text = 'Data de confirmação'
+    fig.layout.yaxis.title.text = 'Quantidade de casos'
+
+    fig.update_layout(
+        margin = dict(l=20, r=20,t=30,b=10)
+    )
+
     return fig
 
 
@@ -51,9 +63,12 @@ def main():
     medias_moveis = carrega_medias_moveis_cidades()
 
     cidades = cidades_do_parana(medias_moveis)
+    
     opcao_cidade = st.sidebar.selectbox('Selecione uma cidade', cidades)
-    figura_cidade = exibe_serie_temporal(medias_moveis, opcao_cidade, 'Casos confirmados')
-    st.pyplot(figura_cidade)
+
+    figura_cidade = exibe_evolucao_casos(medias_moveis, opcao_cidade)
+
+    st.plotly_chart(figura_cidade)
 
 if __name__ == '__main__':
     main()
