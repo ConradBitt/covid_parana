4eec77b141bf5aa75bf55bd465cc1498388043b0
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-08 21:10:40 -0300

    adicionando __main__

--- src/app.py
+++ src/app.py
@@ -3,9 +3,17 @@ import streamlit as st
 from load_data import baixa_base_de_dados_casos_gerais
 import datetime
 
-hoje = datetime.date.today()
 
 st.title('Covid no Estado do Paraná e região')
 
 
+def main():
+    # Baixa base de dados do Governo do Paraná
+    hoje = datetime.date.today()
+    baixa_base_de_dados_casos_gerais(hoje.year, hoje.month, hoje.day - 1)
+
+
+
+if __name__ == '__main__':
+    main()
 
