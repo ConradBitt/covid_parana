d884918975127cbaa98eea95cf45cb05648c2f63
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-08 21:11:25 -0300

    m

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-from load_data import baixa_base_de_dados_casos_gerais
-import datetime
 
 
 st.title('Covid no Estado do Paraná e região')
 
 
 def main():
+
+    from load_data import baixa_base_de_dados_casos_gerais
+    import datetime
     # Baixa base de dados do Governo do Paraná
     hoje = datetime.date.today()
     baixa_base_de_dados_casos_gerais(hoje.year, hoje.month, hoje.day - 1)
