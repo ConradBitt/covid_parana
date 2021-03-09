931f2b335f9c5e3d46df8c3ee7c782437647e7df
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-08 22:48:59 -0300

    m

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import datetime
 
-
 def main():
-    hoje = datetime.date.today()
     st.title('Covid no Estado do Paraná e região')
-    st.text(f'{hoje.day}/{hoje.month}/{hoje.year}',)
+    
 
 
 if __name__ == '__main__':
