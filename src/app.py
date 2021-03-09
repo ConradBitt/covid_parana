6b89ddeeb776131515bb00ef7f80619f76628408
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-09 09:06:13 -0300

    atualizando aplicação

--- src/app.py
+++ src/app.py
@@ -1,10 +1,31 @@
 import streamlit as st
+from informe_covid import InformeCovid
 import datetime
 
-def main():
-    st.title('Covid no Estado do Paraná e região')
-    
 
+# Definições 
+hoje = datetime.date.today()
+
+
+# demora carregar
+#informes = InformeCovid()
+#dados_informe = informes.carrega_informe_mm(hoje)
+
+
+st.dataframe(dados_informe.head())
+
+def apresentacao():
+    st.markdown('''
+        Este trabalho tem como objetivo dar um panorâma sobre a
+        situação sanitária com respeito ao número de casos em
+        cada cidade do estado do paraná. As informações são
+        coletadas da secretaria de saúde do estado.
+    ''')
+
+
+def main():
+    st.title('Covid no Estado do Paraná e região')    
+    apresentacao()
 
 if __name__ == '__main__':
     main()
