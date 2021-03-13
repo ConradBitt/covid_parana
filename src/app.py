ba2af6119e3a6353a6848d6c7d2e5ee2eac3d0b6
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-13 13:11:02 -0300

    adicionando fourrier order 4

--- src/app.py
+++ src/app.py
@@ -133,8 +133,8 @@ def executa_prophet(dataframe, cidade):
     dataframe = dataframe[['DATA_CONFIRMACAO_DIVULGACAO', 'CASO_CONFIRMADO']]
     
     modelo = Prophet()
-    modelo.add_seasonality(name='weekly', period=2,
-     fourier_order=12, mode='multiplicative')
+    modelo.add_seasonality(name='weekly', period=6,
+     fourier_order=4, mode='multiplicative')
     
 
     dataframe.columns = ['ds','y']
