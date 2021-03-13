f450568bdde630d17daf6fcf2704400ba6d0e9b7
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-03-13 13:08:45 -0300

    ajustando sasonalidade semanal prophet

--- src/app.py
+++ src/app.py
@@ -133,7 +133,7 @@ def executa_prophet(dataframe, cidade):
     dataframe = dataframe[['DATA_CONFIRMACAO_DIVULGACAO', 'CASO_CONFIRMADO']]
     
     modelo = Prophet()
-    modelo.add_seasonality(name='monthly', period=2, fourier_order=6)
+    modelo.add_seasonality(name='weekly', period=2, fourier_order=6)
     
 
     dataframe.columns = ['ds','y']
