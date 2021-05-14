e136d6be08666c129339476339840c8aaf93629e
Conrado Bittencourt <conrad.bittencourt@gmail.com>
2021-05-14 13:31:04 -0300

    ajustando datas das internações

--- src/carrega_internacoes.py
+++ src/carrega_internacoes.py
@@ -13,24 +13,21 @@ class CarregaInternacoes():
             municipios.append(unidecode(municipio[7:]).upper())
 
         dataframe['Município'] = municipios
-        dataframe.drop('Total',axis='columns')
-        dataframe = dataframe.T.iloc[:-1]
+        dataframe = dataframe.drop('Total', axis='columns')
+        #dataframe = dataframe.T.iloc[:-1]
 
-        #  Modificar esta linha, pois a cada mês de internação
-        # deve-se adicionar um elemento à tupla
-        index = [
-            'MUN_ATENDIMENTO', '2020-03', '2020-04',
+        columns = [
+            'Município', '2020-03', '2020-04',
             '2020-05', '2020-06', '2020-07',
             '2020-08', '2020-09', '2020-10', 
             '2020-11', '2020-12', '2021-01',
-            '2021-02', '2021-03'
+            '2021-02','2021-03',
             ]
 
-        dataframe.index = index
-        dataframe = dataframe.T
-        dataframe = dataframe.set_index('MUN_ATENDIMENTO')
-        dataframe = dataframe.replace('-',0).astype(int)
+        dataframe.columns = columns
+        dataframe = dataframe.set_index('Município')
         dataframe.columns = pd.to_datetime(dataframe.columns)
+        dataframe = dataframe.replace('-',0).astype(int)
         return dataframe.T
 
     def carregar_internacoes(self):
